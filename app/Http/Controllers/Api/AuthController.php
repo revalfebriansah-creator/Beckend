981a9b7c4f79b05b2ba8b160ea79934e763715e8
revalfebriansah <revalfebriansah@gmail.com>
2026-06-21 18:55:38 +0700

    feat : membuat endpoint  API login dan register

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password) // Enkripsi password
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Registrasi berhasil!',
+            'user' => $user
+        ]);
+    }
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Login berhasil!',
+                'token' => $token,
+                'user' => $user
+            ]);
+        }
+
+        
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Email atau password salah'
+        ], 401);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
